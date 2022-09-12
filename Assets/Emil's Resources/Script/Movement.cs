53bd1fe9a11a261a88a5311e1cc9d3a89692377e
Emil Ernebäck Lehtola <emilgoliat@gmail.com>
2022-09-12 11:08:12 +0200

    fast movement script

--- Assets/Emil's Resources/Script/Movement.cs
+++ Assets/Emil's Resources/Script/Movement.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private GameObject player;
+    private CharacterController controller;
     public Camera playerCamera;
+    public int movementSpeed;
+    
     // Start is called before the first frame update
     void Start()
     {
-        player = GetComponent<GameObject>();
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * movementSpeed);
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
     }
+
 }
+
