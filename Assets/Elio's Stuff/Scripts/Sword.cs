7f229f0b51f0025a02a57c62c8018bedf0bbe314
ElioMio <elio.enestrom@elev.ga.lbs.se>
2022-09-19 09:46:00 +0200

    Weapon basics

--- Assets/Elio's Stuff/Scripts/Sword.cs
+++ Assets/Elio's Stuff/Scripts/Sword.cs
@@ -2,15 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapons : MonoBehaviour
+public class Sword : Weapons
 {
-    public int damage, recoil;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    
+
     // Update is called once per frame
     void Update()
     {
